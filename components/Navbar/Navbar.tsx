02475812768e00cd85ba5394d807d8dc4a89fa5d
Jason Chou <JasonChou0105@gmail.com>
2024-12-30 23:59:47 -0500

    navbar and starting project

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { IoCartOutline, IoPersonOutline } from "react-icons/io5"; // Ionicons from React Icons
+
+function Navbar() {
+  return (
+    <div className="w-full ">
+      <div className="flex flex-row space-between items-center justify-between mb-4 px-8 py-6 bg-black rounded-b-md">
+        <div className="text-white text-3xl">Borger?</div>
+        <div className="flex flex-row justify-center items-center space-between text-white">
+          <div className="mx-8 flex flex-row space-between items-center justify-between">
+            <div className="mx-4">Home</div>
+            <div className="mx-4">Menu</div>
+          </div>
+          <div className="mx-8 flex flex-row space-between items-center justify-between">
+            <IoCartOutline size={24} color="white" className="mx-2" />
+            <div className="border-2 rounded-full aspect-square flex items-center justify-center mx-2">
+              <IoPersonOutline size={20} color="white" className="m-1" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
