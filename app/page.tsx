1b44d1bdf7e67a6161b549e9297c70153824eacb
Jason Chou <JasonChou0105@gmail.com>
2025-01-09 22:13:13 -0500

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Home from "@/components/Home/Home";
 import React from "react";
+import Footer from "@/components/Footer/Footer";
 
 export default function Page() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import { FaRegCopyright } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="flex h-16 w-full justify-center items-center bg-amber-950">
+      <div className="text-white text-sm mr-2">
+        Random footer stuff? i kinda like tailwind now. random copyright sign.
+      </div>
+      <FaRegCopyright color="white" size={12} />
+    </div>
+  );
+}
+
+export default Footer;
