ba168afc92f7daf3b0fd4d0da9727a4a6533632e
Jason Chou <JasonChou0105@gmail.com>
2025-01-09 21:56:30 -0500

    small transtion changes on navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import { IoCartOutline, IoPersonOutline } from "react-icons/io5"; // Ionicons fr
 function Navbar() {
   //test
   return (
-    <div className="w-full">
-      <div className="flex flex-row space-between items-center justify-between px-8 py-6 bg-stone-950 rounded-b-md text-white">
+    <div className="w-full fixed z-30">
+      <div className="flex flex-row space-between items-center justify-between px-8 py-6 bg-stone-900 rounded-b-md text-white">
         <div className=" text-3xl font-black">BORDGER</div>
         <div className="flex flex-row justify-center items-center space-between">
           <div className="mx-8 flex flex-row space-between items-center justify-between">
@@ -13,10 +13,13 @@ function Navbar() {
             <div className="mx-4">Menu</div>
           </div>
           <div className="mx-8 flex flex-row space-between items-center justify-between">
-            <IoCartOutline size={24} color="white" className="mx-2" />
             <div className="border-2 rounded-full aspect-square flex items-center justify-center mx-2">
               <IoPersonOutline size={20} color="white" className="m-1" />
             </div>
+            <button className="mx-2 flex rounded-full bg-amber-600 px-4 py-2 justify-center items-center border-2 border-amber-600 hover:bg-transparent transition-all ease-in-out duration-400">
+              <IoCartOutline size={20} color="white" className="mr-2" />
+              <div className="text-white text-sm">View Cart</div>
+            </button>
           </div>
         </div>
       </div>
