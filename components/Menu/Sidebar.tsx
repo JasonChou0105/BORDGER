0c4d3a3b2cd2dc329ba5719788a9015cbdd746e5
Jason Chou <JasonChou0105@gmail.com>
2025-01-17 14:35:40 -0500

    small tweaks on sidebar animations

--- components/Menu/Sidebar.tsx
+++ components/Menu/Sidebar.tsx
@@ -10,7 +10,7 @@ function Sidebar() {
       <div className="bg-stone-800 rounded-lg flex py-6 px-12 ml-16 my-6">
         <Link
           href={{ pathname: "/menu" }}
-          className="transition-all ease-in duration-400 font-bold text-white text-2xl hover:bg-stone-700 py-2 px-4 rounded-lg"
+          className="font-bold text-white text-2xl transition-all ease-in flex flex-col duration-400 hover:bg-stone-700 hover:scale-105 py-2 px-4 rounded-lg"
         >
           Our Menu
         </Link>
@@ -18,17 +18,16 @@ function Sidebar() {
       <div className="bg-stone-800 rounded-lg flex flex-col text-neutral-300 text-base py-6 px-12 ml-16 my-6">
         {menu.map((item) => {
           return (
-            <div key={item.id} className="my-3">
-              <Link
-                href={{
-                  pathname: "/menu",
-                  query: { itemId: item.id },
-                }}
-                className="transition-all ease-in duration-400 hover:bg-stone-700 py-2 px-4 rounded-lg"
-              >
-                {item.title}
-              </Link>
-            </div>
+            <Link
+              key={item.id}
+              href={{
+                pathname: "/menu",
+                query: { itemId: item.id },
+              }}
+              className="my-1 w-full w-full transition-all ease-in duration-400 hover:bg-stone-700 hover:scale-105 py-2 px-4 rounded-lg "
+            >
+              {item.title}
+            </Link>
           );
         })}
       </div>
